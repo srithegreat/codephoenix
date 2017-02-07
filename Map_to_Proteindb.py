644218cadec4b3ce66a7368dafe158deec12ed6c
srithegreat <srikanth0403@gmail.com>
2017-02-07 15:01:25 +0530

    modified code to read fasta file input

--- Map_to_Proteindb.py
+++ Map_to_Proteindb.py
@@ -13,18 +13,18 @@ c=0
 nf=open("Peptides_not_mapping_refseq.fasta","w")#output filename with unique entries
 out=open("Peptides_matching_all.txt","w") #output filename with_all mappings
 #pepcol=int(raw_input("Enter Column of peptides"))-1
-for record in open(raw_input("Enter filename with peptides: ").strip()):
+for record in SeqIO.parse(open(raw_input("Enter filename with peptides: ").strip()),"fasta"):
 	
-	pep=record.strip()
-	#header=record.description
+	pep=str(record.seq)
+	header=record.description
 	match=[i.start() for i in re.finditer(pep.strip().upper(),s)]
-	#print match
+
 	ind=[(acc[s[:j].count("#")]) for j in match]
 	#print pep,ind
-	#if ind==[]:
-	#nf.write(">"+header+"\n"+pep+"\n")
-	#else:
-	out.write(pep.strip()+"\t"+";".join(ind)+"\n")
+	if ind==[]:
+		nf.write(">"+header+"\n"+pep+"\n")
+	else:
+		out.write(pep.strip()+"\t"+";".join(ind)+"\n")
 	c+=1
 	if c%50==0:print "done",c
 
